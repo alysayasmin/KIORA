1be4c08eca46f8f7fdc8f164becd083dc63edafd
Fitria Alysa Yasmin <alysayasmin1505@gmail.com>
2026-08-09 00:45:11 +0700

    re-deploys

--- KIORA_CMS/src/App.jsx
+++ KIORA_CMS/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router";
 
 import LoginBase from "./layouts/Login";
 import ManagementBase from "./layouts/Management";
@@ -16,8 +16,8 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-
         <Route element={<LoginBase />}>
+          <Route path="/" element={<Navigate to="/login" replace />} />
           <Route path="/login" element={<Login />} />
         </Route>
 
@@ -31,7 +31,6 @@ export default function App() {
         </Route>
 
         <Route path="*" element={<NotFound />} />
-
       </Routes>
     </BrowserRouter>
   );
